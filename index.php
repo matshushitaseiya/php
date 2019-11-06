64a6c9fc54d164b056c334b03e8880b6e29e6f9f
matsushitaseiya <ankle.break.ayies@gmail.com>
2019-11-06 18:03:12 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>php1</title>
+  <link rel="stylesheet" href="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+  echo 'hello world!!';
+
+  ?>
+
+
+</body>
+</html>
